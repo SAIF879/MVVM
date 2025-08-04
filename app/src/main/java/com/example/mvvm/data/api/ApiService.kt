cc18ff0f832a9168be22887fbb2d97ac91c86185
Mohd Saif Ahmed <saifahmed9308@gmail.com>
2025-08-04 12:10:48 +0530

    minor changes

--- app/src/main/java/com/example/mvvm/data/api/ApiService.kt
+++ app/src/main/java/com/example/mvvm/data/api/ApiService.kt
@@ -2,9 +2,11 @@ package com.example.mvvm.data.api
 
 import com.example.mvvm.data.dto.PostsDto
 import retrofit2.Response
+import retrofit2.http.GET
 
 interface ApiService{
 
+    @GET("/posts")
     suspend fun getAllPosts() : Response<List<PostsDto>>
 
 }
